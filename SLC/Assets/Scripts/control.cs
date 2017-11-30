5196be52ca79f919545bf886ae24cbbd78a16aaf
xRoALex <bir.alex10@yahoo.ro>
2017-11-30 16:14:36 +0200

    yay

---
+++ SLC/Assets/Scripts/control.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+    public int turn ;
+    public int speed;
+    public float drift ;
+    private Rigidbody2D rb;
+
+    void Start () {
+		rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate () {
+        if (Input.GetButton("Accelerate"))
+        {
+            rb.AddForce(this.transform.up * speed);
+        }
+        //rb.AddTorque(-Input.GetAxis("Horizontal") * turn);
+        rb.angularVelocity = -Input.GetAxis("Horizontal")*turn;
+        Vector2 forwardVel = transform.up * Vector2.Dot(rb.velocity,transform.up);
+        Vector2 rightVel = transform.right * Vector2.Dot(rb.velocity, transform.right);
+        rb.velocity = forwardVel + rightVel * drift;
+
+    }
+}
